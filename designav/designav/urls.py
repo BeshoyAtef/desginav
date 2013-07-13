6a8c5301b38c86575c0230cd1759795e40f21b34
Tharwat7 <tharwat.amr@gmail.com>
2013-07-13 18:23:44 +0200

    Work so far #16 #13 #12

--- designav/designav/urls.py
+++ designav/designav/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
+from designav_www.views import *
 
 urlpatterns = patterns('',
     # Examples:
@@ -10,8 +11,8 @@ urlpatterns = patterns('',
     # url(r'^designav/', include('designav.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
--- designav/designav_www/views.py
+++ designav/designav_www/views.py
@@ -1,1 +1,32 @@
-# Create your views here.
+from designav_www.models import *
+from django.shortcuts import render_to_response, redirect, render
+from django.http import HttpResponse
+import datetime
+
+def view_members(request):
+
+	list_of_members = []
+	list_of_members = Members.objects.all()
+	print list_of_members
+
+	return render(request, 'members.html', {'list_of_members': list_of_members})
+
+def view_news(request):
+	list_of_news = []
+	list_of_news = News.objects.all()
+
+	return render(request, 'news.html', {'list_of_news': list_of_news})
+
+def view_all_projects(request):
+
+	list_of_projects = []	
+	list_of_projects = Projects.objects.all()
+
+	return render(request, 'projects.html', {'list_of_projects': list_of_projects})
+
+def view_project(request):
+
+	project_id = request.GET['id']
+	desired_project = Project.objects.get(id=project_id)
+
+	return render(request, 'project.html', {'project_info': desired_project})
